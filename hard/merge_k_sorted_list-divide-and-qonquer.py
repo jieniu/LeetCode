b5ad5a7b7483a68d04244a8091f9521cccfbcf48
jieniu <yajie.feng@gmail.com>
2019-03-03 17:04:08 +0800

    merge_k_sorted_list-divide-and-qonquer.py

--- hard/merge_k_sorted_list-divide-and-qonquer.py
+++ hard/merge_k_sorted_list-divide-and-qonquer.py
@@ -11,51 +11,38 @@ class Solution(object):
         l1 = sublists[0]
         l2 = sublists[1]
 
-        ret = None
-        curr = None
-        while l1 is not None or l2 is not None:
-            val = None
-            if l1 is not None and l2 is not None:
-                if l1.val < l2.val:
-                    val = l1.val
-                    l1 = l1.next
-                else:
-                    val = l2.val
-                    l2 = l2.next
-
-            elif l1 is not None:
-                val = l1.val
+        ret = curr = ListNode(0)
+        while l1 and l2:
+            if l1.val < l2.val:
+                curr.next = l1
                 l1 = l1.next
-
             else:
-                val = l2.val
+                curr.next = l2
                 l2 = l2.next
 
-            if curr is None:
-                curr = ListNode(val)
-                ret = curr
-            else:
-                curr.next = ListNode(val)
-                curr = curr.next
+            curr = curr.next
+        if l1:
+            curr.next = l1
+        elif l2:
+            curr.next = l2
 
-        return ret
+        return ret.next
+
 
     def mergeKLists(self, lists):
         """
         :type lists: List[ListNode]
         :rtype: ListNode
         """
-        if len(lists) == 1:
-            return lists[0]
+        result = lists
+        while len(result) > 1:
+            tmp = []
+            for i in range(1+len(result)/2):
+                sublists = result[i*2:i*2+2]
+                if len(sublists) == 0:
+                    continue
+                retlist = self.mergeTwoLists(sublists)
+                tmp.append(retlist)
+            result = tmp
 
-        result = []
-        for i in range(1+len(lists)/2):
-            sublists = lists[i*2:i*2+2]
-            if len(sublists) == 0:
-                continue
-            retlist = self.mergeTwoLists(sublists)
-            result.append(retlist)
-
-        return mergeKLists(result)
-
-
+        return result[0] if len(result) > 0 else result
