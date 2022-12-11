265d75dc3c6ee626a5bfaf48e2aee6f8dc736889
fengyajie <yajie.feng@gmail.com>
2022-12-11 21:06:58 +0800

    704 binary search

---
+++ easy/704_binary_search.cc
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size();
+        while (left < right) {
+            int middle = (left + right) / 2;
+            if (nums[middle] == target) {
+                return middle;
+            } else if (nums[middle] < target) {
+                left = middle + 1;
+            } else {
+                right = middle;
+            }
+        }
+        return -1;
+    }
+};
